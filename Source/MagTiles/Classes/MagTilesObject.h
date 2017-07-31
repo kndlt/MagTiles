317b0929a61c9367918fb0d41cc3c7d4083ee850
Jin Hyuk Cho <jinhyuki@users.noreply.github.com>
2017-07-30 23:35:30 -0700

    New plugin building fine.

---
+++ Source/MagTiles/Classes/MagTilesObject.h
@@ -0,0 +1,41 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/Object.h"
+#include "MagTilesObject.generated.h"
+
+
+/**
+ * Example UStruct declared in a plugin module
+ */
+USTRUCT()
+struct FMagTilesStruct
+{
+	GENERATED_USTRUCT_BODY()
+ 
+	UPROPERTY()
+	FString TestString;
+};
+ 
+
+/**
+ * Example of declaring a UObject in a plugin module
+ */
+UCLASS()
+class UMagTilesObject : public UObject
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+private:
+
+	UPROPERTY()
+	FMagTilesStruct MyStruct;
+
+};
+
+
---
+++ Source/MagTiles/Private/MagTilesObject.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+#include "MagTilesObject.h"
+
+
+UMagTilesObject::UMagTilesObject( const FObjectInitializer& ObjectInitializer )
+	: Super( ObjectInitializer )
+{
+}
