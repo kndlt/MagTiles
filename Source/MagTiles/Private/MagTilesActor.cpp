55c37b1f6d5c59b44dde1f757b0d08bfa3aae7dc
Jin Hyuk Cho <jinhyuki@users.noreply.github.com>
2017-08-01 22:42:34 -0700

    Generated empty actor
    https://github.com/jinhyuki/MagTiles/issues/3

---
+++ Source/MagTiles/Private/MagTilesActor.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MagTiles.h"
+#include "MagTilesActor.h"
+
+
+// Sets default values
+AMagTilesActor::AMagTilesActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMagTilesActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMagTilesActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
