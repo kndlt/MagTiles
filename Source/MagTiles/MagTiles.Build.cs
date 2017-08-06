b4cf47e7595aafa4d4f5813ed0ac0f786d47b356
Jin Hyuk Cho <jinhyuki@users.noreply.github.com>
2017-08-06 15:34:25 -0700

    Replace tabs

--- Source/MagTiles/MagTiles.Build.cs
+++ Source/MagTiles/MagTiles.Build.cs
@@ -2,45 +2,45 @@
 
 namespace UnrealBuildTool.Rules
 {
-	public class MagTiles : ModuleRules
-	{
-		public MagTiles(ReadOnlyTargetRules Target) : base(Target)
-		{
-			PublicIncludePaths.AddRange(
-				new string[] {
-					// ... add public include paths required here ...
-				}
-			);
+    public class MagTiles : ModuleRules
+    {
+        public MagTiles(ReadOnlyTargetRules Target) : base(Target)
+        {
+            PublicIncludePaths.AddRange(
+                new string[] {
+                    // ... add public include paths required here ...
+                }
+            );
 
-			PrivateIncludePaths.AddRange(
-				new string[] {
+            PrivateIncludePaths.AddRange(
+                new string[] {
                     "Developer/MagTiles/Private",
                     // ... add other private include paths required here ...
-				}
-			);
+                }
+            );
 
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
+            PublicDependencyModuleNames.AddRange(
+                new string[]
+                {
                     "Core", "CoreUObject", // Core includes
                     "ShaderCore", "RenderCore", "RHI", "RuntimeMeshComponent", // For RMC
                     "Engine" // For things like actor component
                 }
-			);
+            );
 
-			PrivateDependencyModuleNames.AddRange(
-				new string[]
-				{
-					// ... add private dependencies that you statically link with here ...
-				}
-			);
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    // ... add private dependencies that you statically link with here ...
+                }
+            );
 
-			DynamicallyLoadedModuleNames.AddRange(
-				new string[]
-				{
-					// ... add any modules that your module loads dynamically here ...
-				}
-			);
-		}
-	}
+            DynamicallyLoadedModuleNames.AddRange(
+                new string[]
+                {
+                    // ... add any modules that your module loads dynamically here ...
+                }
+            );
+        }
+    }
 }
